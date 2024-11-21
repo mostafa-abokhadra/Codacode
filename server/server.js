a14ba851ec2b6b7a109d07957e9db0402c4662a4
mostafa-abokhadra <mvxizk@gmail.com>
2024-11-21 15:31:45 +0200

    initiating home page code

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const homeRoute = require('./src/routes/homeRoute')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/src/views'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoute)
+
+app.listen(3000, () => {
+    console.log("i'm listening")
+})
