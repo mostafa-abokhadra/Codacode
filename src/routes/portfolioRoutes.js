20f24d4c043cbd4a52f8fc14c43a7c20b966f0f8
mostafa-abokhadra <mvxizk@gmail.com>
2025-02-13 16:18:14 +0200

    adding new portfolio route and its controller: updatePortfolioAbout section

--- src/routes/portfolioRoutes.js
+++ src/routes/portfolioRoutes.js
@@ -2,9 +2,16 @@ const router = require('express').Router()
 const {ensureAuthenticated} = require('../middlewares/checkSession')
 const portfolioController = require('../controllers/portfolioController')
 
+// getting authenticated user portfolio
+// getting userApplied to projects portfolio
 router.get(
     '/:username/portfolio',
     ensureAuthenticated,
     portfolioController.getPortfolio
+)
+router.put(
+    '/portfolio/about',
+    ensureAuthenticated,
+    portfolioController.updatePortfolioAbout
 )
 module.exports = router
