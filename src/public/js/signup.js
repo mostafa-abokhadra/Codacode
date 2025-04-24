19ac4390eed605bdb47cb1fadc234f6ada3eee08
mostafa-abokhadra <mvxizk@gmail.com>
2025-04-24 19:37:59 +0200

    adding password input field feedback in client side user, adding password constraint to must be contain 1 capital letter, 1 small letter, 1 special character, 1 number, and minimum length of 8 character

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -19,6 +19,50 @@ function CheckIsAlphaNum(value) {
         return 0
     return 1
 }
+
+function containsLowercase(password) {
+    let contains = false // assumning password don't contain small letter
+    for(let i =0; i < password.length; i++) {
+        const charCode = password.charCodeAt(i)
+        if (charCode >= 97 && charCode <= 122) {
+            contains = true
+            break;
+        }
+    }
+    return contains
+}
+function containsUppercase(password) {
+    let contains = false // assumning password don't contain capital letter
+    for(let i =0; i < password.length; i++) {
+        const charCode = password.charCodeAt(i)
+        if (charCode >= 65 && charCode <= 90) {
+            contains = true
+            break;
+        }
+    }
+    return contains
+}
+function containsNumber(password) {
+    let contains = false // assumning password don't contain capital letter
+    for(let i =0; i < password.length; i++) {
+        const charCode = password.charCodeAt(i)
+        if (charCode >= 48 && charCode <= 57) {
+            contains = true
+            break;
+        }
+    }
+    return contains
+}
+function containsSpecialCharacter(password) {
+    let contains = false // assumning password don't contain any digits 
+    for(let i =0; i < password.length; i++) {
+        if (!CheckIsAlphaNum(password[i])) {
+            contains = true
+            break
+        }
+    }
+    return contains
+}
 async function checkFullNameValidity() {
 
     const fullName = fullNameInput.value
@@ -113,6 +157,45 @@ async function checkEmailValidity() {
     }
     return 1
 }
+
+function checkPasswordValidity() {
+    const password = passwordInput.value
+    if (password === '') {
+        console.log('here')
+        feedback(passwordInput, 'password is required')
+        return 0;
+    }
+    if (!containsLowercase(password)) {
+        feedback(passwordInput, 'must contain at least 1 small letter')
+        return 0;
+    }
+    if (!containsUppercase(password)) {
+        feedback(passwordInput, 'must contain at least 1 cpital letter')
+        return 0;
+    }
+    if (!containsSpecialCharacter(password)) {
+        feedback(passwordInput, 'must contain at least 1 special letter')
+        return 0;
+    }
+    if (!containsNumber(password)) {
+        feedback(passwordInput, 'must contain at least 1 number')
+        return 0;
+    }
+    if (password.length < 8) {
+        feedback(passwordInput, 'password should be 8 letters or more')
+        return 0;
+    }
+}
+
+const passwordInput = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('confirm-password')
+passwordInput.addEventListener('input', (e) => {
+    removePreviousFeedbackMessages()
+    checkPasswordValidity()
+    // similarToConfirmationInput
+})
+
+
 const emailInput = document.getElementById('email')
 emailInput.addEventListener('input', async (e) => {
     removePreviousFeedbackMessages()
@@ -131,5 +214,6 @@ singubBtn.addEventListener('click', async (e) => {
     removePreviousFeedbackMessages()
     await checkFullNameValidity()
     await checkEmailValidity()
+    checkPasswordValidity()
 
 })
